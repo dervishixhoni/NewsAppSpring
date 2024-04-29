d40e421d84ab270a3e796ff75c6be2849aed8c09
Xhoni Dervishi <139445849+dervishixhoni@users.noreply.github.com>
2024-04-29 12:17:55 +0200

    Delete src/main/java/com/xhoni/NewsApp/services/env.java

--- src/main/java/com/xhoni/NewsApp/services/env.java
+++
@@ -1,9 +0,0 @@
-package com.xhoni.NewsApp.services;
-
-public class env {
-    static final String fromEmail = "xhonixhoni2013@gmail.com"; //requires valid gmail id
-    static final String password = "rdrnvrpilxozlbjy";
-    static final String[] allowedExtensions= {"png","jpg","jpeg"};
-    public static final String API_KEY = "bf1cb8dc1df74505a310b9dc5301942e";
-}
-
